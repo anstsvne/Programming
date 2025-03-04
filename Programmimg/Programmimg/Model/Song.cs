21038cf7a5067bbf7d87ac42e27c28d1157d7719
anstsvne <142682148+anstsvne@users.noreply.github.com>
2025-03-04 23:29:46 +0700

    Update Song.cs

--- Programmimg/Programmimg/Model/Song.cs
+++ Programmimg/Programmimg/Model/Song.cs
@@ -1,113 +1,88 @@
-﻿using Programming.Model.Classes;
-using System;
 
-/// <summary>
-/// Хранит данные о песне - название песни, имя артиста, название лейбла, длительность песни в секундах.
-/// </summary>
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-class Song
+namespace Programming.Model
 {
-
     /// <summary>
-    /// Название песни для каждого объекта класса.
+    /// Хранит данные о песне.
     /// </summary>
-    private string _songName;
-
-    /// <summary>
-    /// Имя артиста для каждого объекта класса.
-    /// </summary>
-    private string _artistName;
-
-    /// <summary>
-    /// Название лейбла, выпустившего песню, для каждого объекта класса.
-    /// </summary>
-    private string _labelName;
-
-    /// <summary>
-    /// Длительность песни в секундах для каждого объекта класса.
-    /// </summary>
-    private int _durationSeconds;
-
-    /// <summary>
-    /// Возвращает и задаёт название песни. Не может быть пустым полем.
-    /// </summary>
-    public string SongName
+    internal class Song
     {
-        get
+        /// <summary>
+        /// Название песни.
+        /// </summary>
+        private string _nameSong;
+        /// <summary>
+        /// Псевдоним исполнителя.
+        /// </summary>
+        private string _artist;
+        /// <summary>
+        /// Длительность песни (в секундах).
+        /// </summary>
+        private int _durationSong;
+        /// <summary>
+        /// Возвращает и задает длительность песни в секундах. Не может быть отрицательным.
+        /// </summary>
+        public int DurationSong
         {
-            return _songName;
+            get { return _durationSong; }
+            set
+            {
+                if (Validator.AssertOnPositiveValue(value))
+                {
+                    _durationSong = value;
+                }
+            }
         }
-        set
+        /// <summary>
+        /// Возвращает и задает название песни. Не может быть пустым.
+        /// </summary>
+        private string NameSong
         {
-            if (string.IsNullOrWhiteSpace(value))
-                throw new ArgumentException("Name of song cannot be empty or null");
-            _songName = value;
+            get { return _nameSong; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Value can not be empty");
+                }
+                _nameSong = value;
+            }
+        }
+        /// <summary>
+        /// Возвращает и задает псевдоним исполнителя. Не может быть отрицательным.
+        /// </summary>
+        private string Artist
+        {
+            get { return _artist; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Value can not be empty");
+                }
+                _artist = value;
+            }
+        }
+        /// <summary>
+        /// Создает экземпляр класса <see cref="Song"/>
+        /// </summary>
+        /// <param name="nameSong">Название песни. Не может быть пустым.</param>
+        /// <param name="nameArtist">Псевдоним исполнителя. Не может быть пустым.</param>
+        /// <param name="durationSong">Длительность песни. Не может быть отрицательным.</param>
+        public Song(string nameSong, string nameArtist, int durationSong)
+        {
+            NameSong = nameSong;
+            Artist = nameArtist;
+            DurationSong = durationSong;
+        }
+        public Song()
+        {
+
         }
     }
-
-    /// <summary>
-    /// Возвращает и задаёт имя артиста. Не может быть пустым полем.
-    /// </summary>
-    public string ArtistName
-    {
-        get
-        {
-            return _artistName;
-        }
-        set
-        {
-            if (string.IsNullOrWhiteSpace(value))
-                throw new ArgumentException("Artist name cannot be empty or null");
-            _artistName = value;
-        }
-    }
-
-    /// <summary>
-    /// Возвращает и задаёт название лейбла. Не может быть пустым полем.
-    /// </summary>
-    public string LabelName
-    {
-        get
-        {
-            return _labelName;
-        }
-        set
-        {
-            if (string.IsNullOrWhiteSpace(value))
-                throw new ArgumentException("Label name cannot be empty or null");
-            _labelName = value;
-        }
-    }
-
-    /// <summary>
-    /// Возвращает и задаёт длительность песни в секундах. Должно быть целым числом, больше 0.
-    /// </summary>
-    public int DurationSeconds
-    {
-        get
-        {
-            return _durationSeconds;
-        }
-        set
-        {
-            Validator.AssertOnPositiveValue(value, nameof(DurationSeconds));
-            _durationSeconds = value;
-        }
-    }
-
-    /// <summary>
-    /// Создаёт экзмепляр класса <see cref="Song"/>
-    /// </summary>
-    /// <param name="SongName"> Название песни.Не может бытьп пустым. </param>
-    /// <param name="ArtistName"> Имя артиста. Не может быть пустым. </param>
-    /// <param name="LabelName"> Название лейбла. Не может быть пустым. </param>
-    /// <param name="DurationSeconds"> Длительность песни в секундах. Должно быть целым числом, больше 0. </param>
-    public Song(string SongName, string ArtistName, string LabelName, int DurationSeconds)
-    {
-        _songName = SongName;
-        _artistName = ArtistName;
-        _labelName = LabelName;
-        _durationSeconds = DurationSeconds;
-    }
-    public Song() { }
 }
